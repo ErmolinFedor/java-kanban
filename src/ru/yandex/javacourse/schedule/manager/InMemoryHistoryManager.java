e791cc0a4ad13554fde84f64f2a451b7bb1439f0
Fedor Ermolin <fermolin@mtsbank.ru>
2025-10-05 15:44:27 +0300

    added remove and hashMap logic to history manager

--- src/ru/yandex/javacourse/schedule/manager/InMemoryHistoryManager.java
+++ src/ru/yandex/javacourse/schedule/manager/InMemoryHistoryManager.java
@@ -1,9 +1,10 @@
 package ru.yandex.javacourse.schedule.manager;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.HashMap;
 import java.util.List;
 
+import java.util.Map;
 import ru.yandex.javacourse.schedule.tasks.Task;
 
 /**
@@ -12,24 +13,74 @@ import ru.yandex.javacourse.schedule.tasks.Task;
  * @author Vladimir Ivanov (ivanov.vladimir.l@gmail.com)
  */
 public class InMemoryHistoryManager implements HistoryManager {
-	private final LinkedList<Task> history = new LinkedList<>();
-
-	public static final int MAX_SIZE = 10;
+	private final HistoryLinkedList history = new HistoryLinkedList();
 
 	@Override
 	public List<Task> getHistory() {
-		return new ArrayList<>(history);
+		return history.getTasks();
 	}
 
 	@Override
-	public void addTask(Task task) {
-		if (task == null) {
-			return;
-		}
-		history.add(task);
-		if (history.size() > MAX_SIZE) {
-			history.removeFirst();
+	public void add(Task task) {
+		history.linkLast(task);
+	}
+
+	@Override
+	public void remove(int id) {
+		history.removeNode(id);
+	}
+
+	private static class HistoryLinkedList {
+
+		final private Map<Integer, Node<Task>> nodeMap = new HashMap<>();
+		private Node<Task> head;
+		private Node<Task> tail;
+
+		private List<Task> getTasks() {
+			List<Task> tasks = new ArrayList<>();
+			for (Node<Task> node = head; node != null; node = node.next) {
+				tasks.add(node.task);
+			}
+			return tasks;
 		}
 
+		public void linkLast(Task task) {
+			if (nodeMap.containsKey(task.getId())) {
+				removeNode(nodeMap.get(task.getId()));
+			}
+			final Node<Task> oldTail = tail;
+			final Node<Task> newNode = new Node<>(oldTail, task, null);
+			tail = newNode;
+			if (oldTail == null) {
+				head = newNode;
+			} else {
+				oldTail.next = newNode;
+			}
+			nodeMap.put(task.getId(), newNode);
+		}
+
+		private void removeNode(int id) {
+			if (nodeMap.containsKey(id)) {
+				removeNode(nodeMap.get(id));
+			}
+		}
+
+		private void removeNode(Node<Task> node) {
+			final Node<Task> prev = node.prev;
+			final Node<Task> next = node.next;
+			if (prev == null) {
+				head = next;
+			} else {
+				prev.next = next;
+				node.prev = null;
+			}
+			if (next == null) {
+				tail = prev;
+			} else {
+				next.prev = prev;
+				node.next = null;
+			}
+			node.task = null;
+		}
 	}
 }
